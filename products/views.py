cf3fd310e4d59af7075c5f3f5ff36242bcd396e1
David011e <davidoliver011e@gmail.com>
2024-04-16 12:41:22 +0100

    Set up CreateCheckoutSessionView view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
+from django.conf import settings
+from django.http import JsonResponse
+
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
+class CreateCheckoutSessionView(View):
+    def post(self, request, *args, **kwargs):
+        product_id = self.kwargs["pk"]
+        product = Product.objects.get(id=product_id)
+        YOUR_DOMAIN = "http://127.0.0.1:8000"
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': product.price,
+                        'product_data': {
+                            'name': product.name,
+                            'images': product.image,
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": product.id
+            },
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success/',
+            cancel_url=YOUR_DOMAIN + '/cancel/',
+        )
+        return JsonResponse({
+            'id': checkout_session.id
+        })
