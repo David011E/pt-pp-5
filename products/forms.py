932e11860cf427aeb55d2ee380157315f3efd4de
David011e <davidoliver011e@gmail.com>
2024-04-21 23:26:54 +0100

    Update styles and forms

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Product, Category
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
@@ -16,5 +18,22 @@ class ProductForm(forms.ModelForm):
         self.fields['stripe_price_id'].label += ' | User must get this from stripe'  # Add suffix to the price label
         self.fields['price'].label += ' | (In cents)'  # Add suffix to the price label
 
+        # Add CSS classes to form fields
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'border-black rounded-0 form-control'  # Apply form-control class to all fields
+
+    # Define form layout using Crispy Forms
+    helper = FormHelper()
+    helper.form_method = 'post'
+    helper.form_class = 'form mb-2'
+    helper.label_class = 'control-label'
+    helper.field_class = 'form-control'
+    helper.layout = Layout(
+        Field('category'),
+        Field('name'),
+        Field('description'),
+        Field('stripe_price_id'),
+        Field('price'),
+        Field('image'),
+    )
+
