bd188f7152b99948a9cf1ef879aa4bcf45c14278
David011e <davidoliver011e@gmail.com>
2024-04-22 00:16:02 +0100

    Editing Products and added edit and delete buttons for products

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ from .views import StripeWebhookView  # Import your view
 urlpatterns = [
     path('', views.all_services, name='products'),  
     path('<int:product_id>/', views.product_details, name='product_details'),
-    path('add/', views.add_product, name='add_product'),  
+    path('add/', views.add_product, name='add_product'), 
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),   
 ]
--- products/views.py
+++ products/views.py
@@ -202,3 +202,29 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            sweetify.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_details', args=[product.id]))
+        else:
+            sweetify.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        sweetify.info(request, f'You are editing {product.name}')
+
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
