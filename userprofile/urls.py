29bb673bb0ae1895c635bbcd7a0038ea68fe0090
David011e <davidoliver011e@gmail.com>
2024-04-22 01:28:12 +0100

    Fix the image field but got an error on userprofile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views
-from .views import CancelSubscriptionView
+from .views import UserProfileView, CancelSubscriptionView
 
 urlpatterns = [
-    path('', views.UserProfileView.as_view(), name='userprofile'),
-    path('cancel-subscription/<str:subscription_id>/', views.CancelSubscriptionView.as_view(), name='cancel_subscription'),
+    path('', UserProfileView.as_view(), name='userprofile'),
+    path('cancel-subscription/<str:subscription_id>/', CancelSubscriptionView.as_view(), name='cancel_subscription'),
 ]
