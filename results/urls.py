459ec6d92ed5459a3dc6e8b029e4008ee354d31a
David011e <davidoliver011e@gmail.com>
2024-04-22 13:28:12 +0100

    Add Result view, url and template

--- results/urls.py
+++ results/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.results, name='results')
+    path('', views.results, name='results'),
+    path('add/', views.add_review, name='add_review')
 ]
--- results/views.py
+++ results/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ReviewForm
 
 # Create your views here.
 def results(request):
@@ -6,3 +7,15 @@ def results(request):
     A view to return index page
     """
     return render(request, 'results/results.html')
+
+
+def add_review(request):
+    """ Add a review to the store """
+
+    form = ReviewForm()
+    template = 'results/add_review.html'
+    context = {
+        'form': form
+    }
+
+    return render(request, template, context)
