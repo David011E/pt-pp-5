321f1c2c5356dd58e17fc8b2f71751f21ae713cb
David011e <davidoliver011e@gmail.com>
2024-04-23 19:46:00 +0100

    Setup contact, views, template, forms and admin

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .forms import ContactForm
+from .models import Contact 
+
+import sweetify
 
 # Create your views here.
 def contact_me(request):
-    """
-    A view to return index page
-    """
-    return render(request, 'contact/contact.html')
+    """ Contact me form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            sweetify.success(request, 'Your message was successfully sent!')
+            return redirect(reverse('contact'))
+        else:
+            sweetify.error(request, 'Failed to a message.')
+    else:
+        form = ContactForm()
+
+    form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'form': form
+    }
+    return render(request, template, context)
