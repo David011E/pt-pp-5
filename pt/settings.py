4ba97b1850eaf5cf4d766fcad2f23f81851ea7ff
David011e <davidoliver011e@gmail.com>
2024-04-24 13:01:53 +0100

    Sending real emails

--- pt/settings.py
+++ pt/settings.py
@@ -124,6 +124,18 @@ WSGI_APPLICATION = 'pt.wsgi.application'
 
 SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'pt-pp-5@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
